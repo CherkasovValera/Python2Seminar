81caf0a15270a3fa29204c3348d2db945de332f4
Valeriy <cherkasov77@mail.ru>
2022-10-02 21:49:22 +0400

    Start5num

--- Seminar2_5.py
+++ Seminar2_5.py
@@ -1,1 +1,13 @@
 # 2   Реализуйте алгоритм перемешивания списка.
+
+a = [18,86,'wef','git','fet','ddr','sssr','data',65,'<->','wilo',101,'rem',73]
+b = []
+i=0
+print(*a)
+while i<10:
+    d=a[i]
+    a[i]=a[i+4]
+    a[i+4]=d
+    b.append(a[i])
+    i+=1
+print(*a,end='\n')
