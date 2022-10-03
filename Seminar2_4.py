f12540006a7735cc35f02914d0fa596054bfcb7d
Valeriy <cherkasov77@mail.ru>
2022-10-03 23:04:52 +0400

    StartAllnumTXTon

--- Seminar2_4.py
+++ Seminar2_4.py
@@ -10,14 +10,17 @@ data.write('4')
 data.close()
 path = 'file.txt'
 data = open(path, 'r')
+data_list = []
 for line in data:
-    print(line)
+   data_list.append(int(line))
 data.close()
+print(data_list)
 a = int(input('Введите число не менее 3:  '))
 list=[]
 for r in range(-a, a+1):
     list.append(r)
-p1=list[0]*list[2]*list[4]
+    n=len(data_list)
+print(n)
+p1=data_list[0]*data_list[1]*data_list[2]
 print(list, end ='\n ')
 print(list[0],'*',list[2],'*',list[4], '=', p1)
-
